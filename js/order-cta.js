f5b04513c71aa9f4501a91cae2071d4acc8dface
sobinibani <sobin11@naver.com>
2023-09-20 15:18:01 +0900

    🚸 toggle aria-label text of bookmark-button

--- js/order-cta.js
+++ js/order-cta.js
@@ -39,7 +39,9 @@ function toggleOrderCtaBookmark(){
         newCount = newCount + 1
     }
     countSpan.innerHTML = newCount.toLocaleString()
+    countSpan.setAttribute('aria-label' , `북마크 ${newCount.toLocaleString()}회`)
     this.classList.toggle('is-active')
+
 }
 
 orderCtaBookmark.addEventListener('click',toggleOrderCtaBookmark)
