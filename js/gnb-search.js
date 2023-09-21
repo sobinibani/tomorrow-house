ee81ec0d9a18bc56b7adc402c0e6fffe94c445b7
sobinibani <sobin11@naver.com>
2023-09-21 14:24:37 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,10 @@
 const gnbSearch = document.querySelector(".gnb-search")
 const gnbSearchInput = gnbSearch.querySelector("input")
 const gnbSearchHistory = gnbSearch.querySelector(".search-history")
+const gnbSearchHistoryList = gnbSearchHistory.querySelector("ol")
+
+const deleteAllButton = gnbSearchHistory.querySelector(".search-history-header button")
+
 
 function closeGnbSearchHistoryOnClickOutside(e){
     console.log('111') 
@@ -11,11 +15,22 @@ function closeGnbSearchHistoryOnClickOutside(e){
 }
 
 function openGnbSearchHistory() {
-    gnbSearchHistory.classList.add("is-active") // gnb히스토리 열림
+    if (gnbSearchHistoryList.children.length === 0){
+        return // 아무것도 실행안한단 뜻
+    } else {
+        gnbSearchHistory.classList.add("is-active") // gnb히스토리 열림
+    }
+
     if(gnbSearchHistory.classList.contains("is-active")){ // gnb 히스토리에 is-active 상태라면
         window.addEventListener('click',closeGnbSearchHistoryOnClickOutside)
         // 윈도우 바깥영역 눌렀을때 처리기능이 실행
     }
 }
 
+function deleteAllHistory() {
+    gnbSearchHistoryList.innerHTML = ""
+    gnbSearchHistory.classList.remove("is-active") 
+}
+
 gnbSearchInput.addEventListener("focus",openGnbSearchHistory)
+deleteAllButton.addEventListener("click",deleteAllHistory)
