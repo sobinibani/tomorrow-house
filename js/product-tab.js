0b3a2be825ead7f43a6d6330db6ee3b9ab6ffa92
sobinibani <sobin11@naver.com>
2023-09-21 22:35:48 +0900

    🚧 Detect positions of tab-panels

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,7 +1,9 @@
 const productTab = document.querySelector(".product-tab")
 const productTabButtonList =productTab.querySelectorAll("button")
-
 let currentActiveTab = productTab.querySelector('.is-active')
+
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
 
 function toggleActiveTab(){
     const tabItem = this.parentNode
@@ -13,6 +15,45 @@ function toggleActiveTab(){
     }
 }
 
+function scrollToTabpanel(){
+    const tabpanelId = this.parentNode.getAttribute('aria-labelledby')
+    const tabpanel = document.querySelector(`#${tabpanelId}`)
+    const scrollAmount = 
+    tabpanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+    window.scrollBy({
+        top: scrollAmount,
+        behavior: 'smooth'
+    })
+}
+
 productTabButtonList.forEach((button)=>{
     button.addEventListener("click",toggleActiveTab)
+    button.addEventListener("click",scrollToTabpanel)
 })
+
+const productTabpanelIDList = [
+    'product-spec',
+    'product-review',
+    'product-inquiry',
+    'product-shipment',
+    'product-recommendation'
+]
+
+const productTabpanelList = productTabpanelIDList.map((panelID)=>{
+    const tabPanel = document.querySelector(`#${panelID}`)
+    return tabPanel
+})
+
+const productTabPositionMap = {}
+
+function detectTabPanelPosition(){
+    productTabpanelList.forEach((panel)=>{
+        const id = panel.getAttribute('id')
+        const position = window.scrollY + panel.getBoundingClientRect().top
+        productTabPositionMap[id] = position
+    })
+}
+
+window.addEventListener('load',detectTabPanelPosition)
+window.addEventListener('resize',detectTabPanelPosition)
