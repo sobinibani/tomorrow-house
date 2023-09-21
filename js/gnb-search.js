4f894469d919666d6dae8e4fa4b4a24fc59e8698
sobinibani <sobin11@naver.com>
2023-09-21 15:00:55 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -4,13 +4,18 @@ const gnbSearchHistory = gnbSearch.querySelector(".search-history")
 const gnbSearchHistoryList = gnbSearchHistory.querySelector("ol")
 
 const deleteAllButton = gnbSearchHistory.querySelector(".search-history-header button")
+const deleteButton = gnbSearchHistoryList.querySelectorAll(".delete-button")
+console.log(deleteButton)
 
+function closeGnbSearchHistory(){
+    gnbSearchHistory.classList.remove('is-active')
+    window.removeEventListener("click",closeGnbSearchHistoryOnClickOutside)
+}
 
 function closeGnbSearchHistoryOnClickOutside(e){
     console.log('111') 
-    if(!gnbSearch.contains(e.target)){ // 윈도우가 gnbsearch를 누르지 ! 않았다면? = 바깥영역을 눌렀다면
-        gnbSearchHistory.classList.remove("is-active") // is-active 제거 (gnb히스토리 닫힘)
-        window.removeEventListener("click",closeGnbSearchHistoryOnClickOutside) // 이 함수도 제거 
+    if(!gnbSearch.contains(e.target)){
+        closeGnbSearchHistory()
     }
 }
 
@@ -18,19 +23,32 @@ function openGnbSearchHistory() {
     if (gnbSearchHistoryList.children.length === 0){
         return // 아무것도 실행안한단 뜻
     } else {
-        gnbSearchHistory.classList.add("is-active") // gnb히스토리 열림
+        gnbSearchHistory.classList.add("is-active") 
     }
 
-    if(gnbSearchHistory.classList.contains("is-active")){ // gnb 히스토리에 is-active 상태라면
+    if(gnbSearchHistory.classList.contains("is-active")){ 
         window.addEventListener('click',closeGnbSearchHistoryOnClickOutside)
-        // 윈도우 바깥영역 눌렀을때 처리기능이 실행
     }
 }
 
 function deleteAllHistory() {
     gnbSearchHistoryList.innerHTML = ""
-    gnbSearchHistory.classList.remove("is-active") 
+    closeGnbSearchHistory()
 }
+
+function deleteSearchHistoryItem(e){
+    e.stopPropagation()
+    const itemToDelete = this.parentNode
+    gnbSearchHistoryList.removeChild(itemToDelete)
+    
+    if (gnbSearchHistoryList.children.length === 0){
+        closeGnbSearchHistory()
+    } 
+}
+
+deleteButton.forEach((button)=>{
+    button.addEventListener("click",deleteSearchHistoryItem)
+})
 
 gnbSearchInput.addEventListener("focus",openGnbSearchHistory)
 deleteAllButton.addEventListener("click",deleteAllHistory)
